f98fe9349dc6cfa4d1a74130d8528c955ee2a118
David Biggs <david@Davids-MacBook-Pro.local>
2021-01-31 12:03:29 -0600

    Added a readme, updates to appearance, and javascript refactoring.

---
+++ README.md
@@ -0,0 +1,7 @@
+# Palindrome Checker
+
+This is a part of my FreeCodeCamp [Javascript Algorithms and Data Structures Certification](https://www.freecodecamp.org/learn). It's the first project in the module. The code passes all of the tests in the [project requirements](https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/javascript-algorithms-and-data-structures-projects/palindrome-checker).
+
+## Usage
+
+Download the project to your machine and open index.html in a web browser.
--- index.js
+++ index.js
@@ -1,24 +1,40 @@
+const form = document.querySelector("#palindrome-form");
+const input = document.querySelector("#palindrome-input");
 const palindromeResult = document.querySelector("#palindrome-result");
 
-function isPalindrome(str) {
-  const newStr = str.replace(/[^A-Za-z0-9]/g, "");
-  console.log(newStr.toLowerCase());
-  return (
-    newStr.toLowerCase().split("").reverse().join("") === newStr.toLowerCase()
-  );
-}
+form.addEventListener("submit", checkPalindrome, false);
+input.addEventListener("focus", setFocusDirections, false);
+input.addEventListener("blur", setBlurDirections, false);
 
+// function executed for its side effects
 function checkPalindrome(event) {
+  const inputValue = event.srcElement[0].value;
   palindromeResult.textContent = "Checking...";
+
   setTimeout(() => {
-    if (isPalindrome(event.srcElement[0].value)) {
+    if (inputValue.length === 0) {
+      palindromeResult.textContent = "You should probably type something.";
+    } else if (isPalindrome(inputValue)) {
       palindromeResult.textContent = "This is a palindrome.";
     } else {
       palindromeResult.textContent = "Not a palindrome!";
     }
-  }, 500);
+  }, 200);
+
+  // pure function, determines if something is a palindrome
+  function isPalindrome(str) {
+    const newStr = str.replace(/[^A-Za-z0-9]/g, "");
+    console.log(newStr.toLowerCase());
+    return (
+      newStr.toLowerCase().split("").reverse().join("") === newStr.toLowerCase()
+    );
+  }
 }
 
-document
-  .querySelector("#palindrome-form")
-  .addEventListener("submit", checkPalindrome, false);
+function setFocusDirections() {
+  palindromeResult.textContent = "Press enter to evaluate.";
+}
+
+function setBlurDirections() {
+  palindromeResult.textContent = "Press tab.";
+}
