5777718caedcad6f7af9a4859fb264b15aafe6e8
David Biggs <david@Davids-MacBook-Pro.local>
2021-01-31 01:37:20 -0600

    Added palindrome checking javascript functionality

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
-function isPalindrome(str) {}
+const palindromeResult = document.querySelector("#palindrome-result");
+
+function isPalindrome(str) {
+  const newStr = str.replace(/[^A-Za-z0-9]/g, "");
+  console.log(newStr.toLowerCase());
+  return (
+    newStr.toLowerCase().split("").reverse().join("") === newStr.toLowerCase()
+  );
+}
 
 function checkPalindrome(event) {
-  console.log(event);
-  //   console.log(event);
+  palindromeResult.textContent = "Checking...";
+  setTimeout(() => {
+    if (isPalindrome(event.srcElement[0].value)) {
+      palindromeResult.textContent = "This is a palindrome.";
+    } else {
+      palindromeResult.textContent = "Not a palindrome!";
+    }
+  }, 500);
 }
 
 document
